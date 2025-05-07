d9b35797bc65f95e9182d166c01c0f25cb8eb7f2
Rian Benedito <rian.bene14@gmail.com>
2025-05-07 19:04:40 -0300

    Update HTML/CSS/JS

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Site carregado.");
+var memeButton = document.getElementById('memeButton').addEventListener('click', function(){
+    const memeContainer = document.getElementById('memeContainer');
+    
+    if (memeContainer.classList.contains('hidden')) {
+        memeContainer.classList.remove('hidden');
+        this.textContent = 'Ocultar reação';
+    } else {
+        memeContainer.classList.add('hidden');
+        this.textContent = 'Minha honesta reação';
+    }
+})
